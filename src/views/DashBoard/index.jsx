4889dc570f8cf5423c4b42170e1d43da78f34e3e
ajxcj <1597237414@qq.com>
2022-11-10 16:59:05 +0800

    React蛋糕后台管理系统

--- src/views/DashBoard/index.jsx
+++ src/views/DashBoard/index.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/style-prop-object */
 import React, { useEffect } from 'react'
 import './index.less'
 import axios from 'axios'
+// import Logo from './1.JPG'
 export default function DashBoard() {
     useEffect(() => {
         axios.get('/hehe/product/home').then(res => {
@@ -11,6 +13,7 @@ export default function DashBoard() {
         <div className='box'>
             <span>DashBoard</span>
             <strong>1111</strong>
+            {/* <img src={Logo} alt="" style={{ width: '100px', height: '100px' }} /> */}
         </div>
     )
 }
